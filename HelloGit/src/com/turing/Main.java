86c3f3e9d02e5be607a2e0897eafffb12822ea43
jialuo <1057999620@qq.com>
2019-10-24 15:28:33 +0800

    项目修改了

--- HelloGit/src/com/turing/Main.java
+++ HelloGit/src/com/turing/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello Git");
+
+        for(int i=0;i<10;i++){
+            System.out.println("hello"+i);
+        }
     }
 }
